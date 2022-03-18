8f8dee354ef34246c26cd96807d2aacfcdfd99de
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2022-03-19 00:13:28 +0100

    feat: read spf

--- public/decoder.js
+++ public/decoder.js
@@ -1,39 +1,4 @@
-importScripts("https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js")
 importScripts("https://cdnjs.cloudflare.com/ajax/libs/pdf-lib/1.17.1/pdf-lib.min.js")
-
-const JsonFormatter = {
-	stringify: function (cipherParams) {
-	  // create json object with ciphertext
-	  const jsonObj = {
-		ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64),
-	  };
-	  // optionally add iv or salt
-	  if (cipherParams.iv) {
-		jsonObj.iv = cipherParams.iv.toString();
-	  }
-	  if (cipherParams.salt) {
-		jsonObj.s = cipherParams.salt.toString();
-	  }
-	  // stringify json object
-	  return JSON.stringify(jsonObj);
-	},
-	parse: function (jsonStr){
-	  // parse json string
-	  const jsonObj = JSON.parse(jsonStr);
-	  // extract ciphertext from json object, and create cipher params object
-	  const cipherParams = CryptoJS.lib.CipherParams.create({
-		ciphertext: CryptoJS.enc.Base64.parse(jsonObj.ct),
-	  });
-	  // optionally extract iv or salt
-	  if (jsonObj.iv) {
-		cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv);
-	  }
-	  if (jsonObj.s) {
-		cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s);
-	  }
-	  return cipherParams;
-	},
-};
 
 
 const getPage = async (pdfString, metadata, page) => {
@@ -56,27 +21,17 @@ const getPage = async (pdfString, metadata, page) => {
 }
 
 
-const decryptData = (encryptedData) => {
-	const encrypted = JsonFormatter.parse(encryptedData);
-	const decrypted = CryptoJS.AES.decrypt(encrypted, "hello world", {
-	  format: JsonFormatter,
-	});
-	return decrypted.toString(CryptoJS.enc.Utf8);
-};
-
-
-
 onmessage = function(data) {
 
-	const stringBuffer = new Uint8Array(data.data.file)
+	const mediaFileBuffer = new Uint8Array(data.data.file)
 	const pageNumber = data.data.page;
 	const metaDataToBuffer = new Uint8Array(data.data.metadata)
 	const metadata = JSON.parse(new TextDecoder().decode(metaDataToBuffer))
-	const stringifiedJson = new TextDecoder().decode(stringBuffer)
-	getPage(decryptData(stringifiedJson), metadata, pageNumber ).then((data) => {
+
+	const mediaData = new TextDecoder().decode(mediaFileBuffer)
+	getPage(mediaData, metadata, pageNumber ).then((data) => {
 		postMessage(data);
 	})
 	
 };
-
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,56 +14,106 @@ import * as pdfjs from "pdfjs-dist/legacy/build/pdf.js";
 import * as pdfViewer from "pdfjs-dist/web/pdf_viewer.js";
 import "pdfjs-dist/web/pdf_viewer.css";
 import "pdfjs-dist/";
-import { PDFDocument } from "pdf-lib";
+import CryptoJS from "crypto-js";
 import axios from "axios";
+import { PDFDocument } from "pdf-lib";
+import { PDFDocumentProxy } from "pdfjs-dist/";
 
 export default defineComponent({
   setup() {
-    const fileContent = ref();
+    type CipherParams = {
+      ct: string;
+      iv?: string;
+      s?: string;
+    };
+    const JsonFormatter = {
+      stringify: function (cipherParams: CryptoJS.lib.CipherParams) {
+        // create json object with ciphertext
+        const jsonObj: CipherParams = {
+          ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64),
+        };
+        // optionally add iv or salt
+        if (cipherParams.iv) {
+          jsonObj.iv = cipherParams.iv.toString();
+        }
+        if (cipherParams.salt) {
+          jsonObj.s = cipherParams.salt.toString();
+        }
+        // stringify json object
+        return JSON.stringify(jsonObj);
+      },
+      parse: function (jsonStr: string) {
+        // parse json string
+        const jsonObj = JSON.parse(jsonStr);
+        // extract ciphertext from json object, and create cipher params object
+        const cipherParams = CryptoJS.lib.CipherParams.create({
+          ciphertext: CryptoJS.enc.Base64.parse(jsonObj.ct),
+        });
+        // optionally extract iv or salt
+        if (jsonObj.iv) {
+          cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv);
+        }
+        if (jsonObj.s) {
+          cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s);
+        }
+        return cipherParams;
+      },
+    };
 
-    const currentPage = ref<Uint8Array>();
+    const decryptData = (encryptedData: string, decoder: string) => {
+      const encrypted = JsonFormatter.parse(encryptedData);
+      const decrypted = CryptoJS.AES.decrypt(encrypted, decoder, {
+        format: JsonFormatter,
+      });
+
+      return decrypted.toString(CryptoJS.enc.Utf8);
+    };
+
+    const fileContent = ref();
 
     const totalPages = ref(0);
 
-    axios.get("http://localhost:8080/first_book.spdf").then((response) => {
-      fileContent.value = response.data;
-    });
+    const newPdfData = ref();
 
-    const decoder = new Worker("decoder.js");
+    const mainpdfViewerContainer = ref();
 
-    decoder.addEventListener(
-      "message",
-      function (e) {
-        currentPage.value = e.data;
-      },
-      false
-    );
+    const mainPdfDocument = ref<PDFDocumentProxy>();
 
-    const pageNumber = ref(0);
+    axios
+      .get("http://localhost:8080/52DcMvYD2sgXD14E4DhQdms4KyDZA2sKQXx5zosxuNNN.spf")
+      .then((response) => {
+        fileContent.value = response.data;
+      });
+
+    const pageNumber = ref(1);
+
+    const createPDF = async (pdfString: string) => {
+      const currentPdf = await PDFDocument.load(pdfString);
+      const pdfBytes = await currentPdf.save();
+      return pdfBytes;
+    };
 
     const initiateSpdf = async () => {
-      const SpdfData = new TextEncoder().encode(JSON.stringify(fileContent.value.pages));
+      const decoderKey =
+        fileContent.value.pubKey + "Gn5GkKQtjxuZgYBMQKxqDN3VRLyhab5Qt526wqgmRouK";
 
-      const pdfMetaData = new TextEncoder().encode(
-        JSON.stringify(fileContent.value.metadata)
+      const decoderData = decryptData(
+        JSON.stringify(fileContent.value.decoder),
+        decoderKey
       );
+
+      const decordedMedia = `decryptData(
+        JSON.stringify(fileContent.value.pages),
+        ${decoderData}
+      )`;
+
+      const mediaDataString = eval(decordedMedia);
 
       totalPages.value = fileContent.value.metadata.pageCount;
 
-      decoder.postMessage(
-        { file: SpdfData.buffer, metadata: pdfMetaData.buffer, page: pageNumber.value },
-        [SpdfData.buffer, pdfMetaData.buffer]
-      );
-    };
+      newPdfData.value = await createPDF(mediaDataString);
 
-    const loadPage = async () => {
-      if (currentPage.value) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        loadDocument(currentPage.value);
-      } else {
-        initiateSpdf();
-      }
+      loadDocument(newPdfData.value);
     };
 
     const loadDocument = (pdfData: Uint8Array) => {
@@ -75,20 +125,7 @@ export default defineComponent({
       const CMAP_URL = "pdfjs-dist/cmaps/";
       const CMAP_PACKED = true;
 
-      const PAGE_TO_VIEW = 1;
-      const SCALE = 1.0;
-
       const ENABLE_XFA = true;
-
-      if (document.querySelector("#pdf-wrapper") != null) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        document.querySelector("#pdf-wrapper").innerHTML = "";
-      }
-
-      const container = document.querySelector("#pdf-wrapper");
-
-      const eventBus = new pdfViewer.EventBus();
 
       // Loading document.
       const loadingTask = pdfjs.getDocument({
@@ -98,38 +135,54 @@ export default defineComponent({
         enableXfa: ENABLE_XFA,
       });
 
-      (async function () {
-        const pdfDocument = await loadingTask.promise;
+      (async () => {
+        await loadingTask.promise.then(async (pdfDocument) => {
+          const container = document.querySelector("#pdf-wrapper");
 
-        // Document loaded, retrieving the page.
-        const pdfPage = await pdfDocument.getPage(PAGE_TO_VIEW);
-        // Creating the page view with default parameters.
-        const pdfPageView = new pdfViewer.PDFPageView({
+          const SCALE = 1.0;
+
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
-          container,
-          id: PAGE_TO_VIEW,
-          scale: SCALE,
-          defaultViewport: pdfPage.getViewport({ scale: SCALE }),
-          eventBus,
-          // We can enable text/annotation/xfa/struct-layers, as needed.
+          window.pdfMainDocument = pdfDocument;
+
+          const eventBus = new pdfViewer.EventBus();
+
+          // Document loaded, retrieving the page.
+          const pdfPage = await pdfDocument.getPage(1);
+          // Creating the page view with default parameters.
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
-          textLayerFactory: !pdfDocument.isPureXfa
-            ? new pdfViewer.DefaultTextLayerFactory()
-            : null,
-          annotationLayerFactory: new pdfViewer.DefaultAnnotationLayerFactory(),
+          window.pdfViewerContainer = new pdfViewer.PDFPageView({
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            container,
+            id: 1,
+            scale: SCALE,
+            defaultViewport: pdfPage.getViewport({ scale: SCALE }),
+            eventBus,
+            // We can enable text/annotation/xfa/struct-layers, as needed.
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            textLayerFactory: !pdfDocument.isPureXfa
+              ? new pdfViewer.DefaultTextLayerFactory()
+              : null,
+            annotationLayerFactory: new pdfViewer.DefaultAnnotationLayerFactory(),
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            xfaLayerFactory: pdfDocument.isPureXfa
+              ? new pdfViewer.DefaultXfaLayerFactory()
+              : null,
+            structTreeLayerFactory: new pdfViewer.DefaultStructTreeLayerFactory(),
+          });
+
+          // Associate the actual page with the view, and draw it.
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
-          xfaLayerFactory: pdfDocument.isPureXfa
-            ? new pdfViewer.DefaultXfaLayerFactory()
-            : null,
-          structTreeLayerFactory: new pdfViewer.DefaultStructTreeLayerFactory(),
+          window.pdfViewerContainer.setPdfPage(pdfPage);
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          return window.pdfViewerContainer.draw();
         });
-
-        // Associate the actual page with the view, and draw it.
-        pdfPageView.setPdfPage(pdfPage);
-        return pdfPageView.draw();
       })();
     };
 
@@ -139,7 +192,7 @@ export default defineComponent({
     };
 
     const goToNextPrev = () => {
-      if (pageNumber.value == 0) return;
+      if (pageNumber.value == 1) return;
       pageNumber.value--;
     };
 
@@ -147,12 +200,16 @@ export default defineComponent({
       await initiateSpdf();
     });
 
-    watch(currentPage, async () => {
-      await loadPage();
-    });
-
     watch(pageNumber, async () => {
-      currentPage.value = undefined;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const page = await window.pdfMainDocument.getPage(pageNumber.value);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      window.pdfViewerContainer.setPdfPage(page);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      window.pdfViewerContainer.draw();
     });
 
     return {
